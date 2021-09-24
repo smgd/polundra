5fc2082ef5c83dcf11a1e0cccdf0c2c036fe67db
Artur Svistunov <18216480+madbird1304@users.noreply.github.com>
2021-09-24 04:56:19 +0300

    WIP

--- disco.py
+++ disco.py
@@ -1,8 +1,8 @@
 from decimal import Decimal
 from pathlib import Path
 from typing import Type, Any
-
 import asyncio
+import dbus
 import math
 import os
 import pulsectl
@@ -12,46 +12,82 @@ import uuid
 import wave
 
 
-class FileVar:
-    def __init__(self, path: str, type_: Type[Any] = str) -> None:
-        self.path: Path = Path(path)
-        self.type_ = type_
-
-    def get(self):
-        return self.type_(self.path.read_text())
-
-    def set(self, value):
-        return self.path.write_text(str(value))
-
-
-class FileProperty:
-    def __init__(self, var: FileVar) -> None:
-        self.var = var
-
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        return self.var.get()
-
-    def __set__(self, instance, value):
-        self.var.set(value)
-
-
-class Brightness:
-    maximal = FileProperty(FileVar('/sys/class/backlight/intel_backlight/max_brightness', int))
-    current = FileProperty(FileVar('/sys/class/backlight/intel_backlight/brightness', int))
+DBUS_BACKENDS = {
+    'systemd': {
+        'name': 'org.freedesktop.login1',
+        'path': '/org/freedesktop/login1/session/auto',
+        'interface': 'org.freedesktop.login1.session',
+        'getter': 'GetBrightness',
+        'setter': 'SetBrightness',
+        # 'max_getter': 'GetMaxBrightness',
+    },        
+    'gnome': {
+        'name': 'org.gnome.SettingsDaemon.Power',
+        'path': '/org/gnome/SettingsDaemon/Power',
+        'interface': 'org.gnome.SettingsDaemon.Power.Screen',
+        'getter': 'GetPercentage',
+        'setter': 'SetPercentage',
+    },
+    'upower': {
+        'name': 'org.freedesktop.UPower',
+        'path': '/org/freedesktop/UPower/KbdBacklight',
+        'interface': 'org.freedesktop.UPower.KbdBacklight',
+        'getter': 'GetBrightness',
+        'setter': 'SetBrightness',
+        'max_getter': 'GetMaxBrightness',
+    },
+}
 
 
+class DBusBrightnessManager:
+    def __init__(self, name, path, interface, getter, setter, max_getter=None) -> None:
+        self.bus = dbus.SystemBus()
+        self.proxy = self.bus.get_object(name, path)
+        self.iface = dbus.Interface(self.proxy, interface)
+        self.getter = getattr(self.iface, getter)
+        self.setter = getattr(self.iface, setter)
+        self.max_getter = getattr(self.iface, max_getter) if max_getter else (lambda: 100)
+        
+    @property
+    def current(self) -> int:
+        return self.getter()
+
+    @current.setter
+    def current(self, value: int) -> None:
+        return self.setter(value)
+
+    @property
+    def maximum(self) -> int:
+        return self.max_getter()
 
     @property
     def value(self) -> Decimal:
-        return Decimal(self.current) / Decimal(self.maximal)
+        return Decimal(self.current) / Decimal(self.maximum)
 
     @value.setter
     def value(self, v) -> None:
-        self.current = int(v * self.maximal)
+        self.current = int(v * self.maximum)
 
 
+class Brightness:
+    def __init__(self):
+        self.kbd = DBusBrightnessManager(**DBUS_BACKENDS['upower'])
+        self.scr = DBusBrightnessManager(**DBUS_BACKENDS['systemd'])
+
+    @property
+    def value(self):
+        return self.scr.value
+
+    @value.setter
+    def value(self, v):
+        try:
+            self.kbd.value = v
+        except:
+            pass
+        try:
+            self.scr.value = v
+        except:
+            pass
 
 def f(v):
     return 0.05 + 0.95 * (0.5 + 0.45 * math.sin(2 * math.pi * v))
@@ -115,6 +151,7 @@ def toggle(event):
 
 async def main():
     event = asyncio.Event()
+    event.set()
     loop = asyncio.get_running_loop()
     loop.add_signal_handler(signal.SIGALRM, lambda *_: toggle(event))
     await asyncio.gather(audio_alert(event), visual_alert(event))
